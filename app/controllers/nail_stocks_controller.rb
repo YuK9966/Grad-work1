4497b111cf1e0321063018fe3d7d2ddd3d3fe23c
矢野優佳里 <yano@Onigawaras-Macbook.local>
2025-11-27 14:57:05 +0900

    add:views/layouts/_flash.html.erb生成、同dir/applicationからフラッシュメッセージを分離。

--- app/controllers/nail_stocks_controller.rb
+++ app/controllers/nail_stocks_controller.rb
@@ -2,10 +2,12 @@ class NailStocksController < ApplicationController
   def create
     @nail_item = NailItem.find(params[:nail_item_id])
     current_user.add_nail_stocks(@nail_item)
+    flash.now[:success] = "#{@nail_item.brand.name} #{ @nail_item.product.name } #{ @nail_item.prod_color.name }をあなたのNail Stocksに追加しました"
   end
 
   def destroy
     @nail_item = current_user.nail_stocks.find(params[:id]).nail_item
     current_user.rm_nail_stocks(@nail_item)
+    flash.now[:success] = "#{@nail_item.brand.name} #{ @nail_item.product.name } #{ @nail_item.prod_color.name }をあなたのNail Stocksから削除しました"
   end
 end
