3c09bb1715c71fa21243162b640b1257d2e3f5d9
矢野優佳里 <yano@Onigawaras-Macbook.local>
2025-11-27 14:34:04 +0900

    add:_header.html.erbにz-index追加、nail_stocksトグルボタンのAjax化

--- app/controllers/nail_stocks_controller.rb
+++ app/controllers/nail_stocks_controller.rb
@@ -2,12 +2,10 @@ class NailStocksController < ApplicationController
   def create
     @nail_item = NailItem.find(params[:nail_item_id])
     current_user.add_nail_stocks(@nail_item)
-    redirect_to nail_items_path, notice: "#{@nail_item.brand.name} #{ @nail_item.product.name } #{ @nail_item.prod_color.name }をあなたのNail Stocksに追加しました"
   end
 
   def destroy
     @nail_item = current_user.nail_stocks.find(params[:id]).nail_item
     current_user.rm_nail_stocks(@nail_item)
-    redirect_to nail_items_path, notice: "#{@nail_item.brand.name} #{ @nail_item.product.name } #{ @nail_item.prod_color.name }をあなたのNail Stocksから削除しました", status: :see_other
   end
 end
