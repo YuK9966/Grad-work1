7d150177c17bff234b50126bf302f916599bc631
矢野優佳里 <yano@Onigawaras-Macbook.local>
2025-12-13 11:48:47 +0900

    add:naillogsに使用アイテム追加機能の実装

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import LogNailsController from "./log_nails_controller"
+application.register("log-nails", LogNailsController)
--- app/javascript/controllers/log_nails_controller.js
+++ app/javascript/controllers/log_nails_controller.js
@@ -1,15 +1,21 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["row"]
+  static targets = ["row", "rows"]
+  static values = { options: String }
+
+  connect() {
+}
 
   add(event) {
     event.preventDefault()
 
+    // 1. 現在の row の数を取得
     const lastIndex = this.rowTargets.length
+    // 2. 次の行の HTML を生成
     const template = this._buildTemplate(lastIndex)
-
-    this.element.insertAdjacentHTML("beforeend", template)
+    // 3. controller （= #log-nails-container）の最後に HTML を追加
+    this.rowsTarget.insertAdjacentHTML("beforeend", template)
   }
 
   remove(event) {
@@ -18,11 +24,15 @@ export default class extends Controller {
   }
 
   _buildTemplate(index) {
+    // Rails から渡された <option> 一式を取得
+    const options = this.optionsValue
+
     return `
-      <div class="log-nail-item flex gap-2 mb-2" data-log-nails-target="row">
-        <select name="naillog[log_nails_attributes][${index}][nail_item_id]"
-                class="select w-full text-sm">
-          ${this._options()}
+      <div class="log-nail-item flex gap-2 mb-1" data-log-nails-target="row">
+        <select name="naillog[log_nails_attributes][${index}][nail_item_id]",
+                 class="select w-full text-xs">
+          <option value="">--</option>
+          ${options}
         </select>
         <button type="button"
                 data-action="log-nails#remove"
