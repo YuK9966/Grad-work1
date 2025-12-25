f215aec0237710684d9321db8c0dd688eee6b4dc
矢野優佳里 <yano@Onigawaras-Macbook.local>
2025-12-25 17:20:41 +0900

    fix:lint check

--- app/models/color.rb
+++ app/models/color.rb
@@ -1,7 +1,7 @@
 class Color < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :color_code, presence: true, uniqueness: true
-  
+
   has_many :log_colors, dependent: :destroy
   has_many :naillogs, through: :log_colors
   has_many :nail_items, dependent: :nullify
