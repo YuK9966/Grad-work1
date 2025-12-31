848e3495189e6d2d53ee3ed4c2115cb0917dcfd3
矢野優佳里 <yano@Onigawaras-Macbook.local>
2025-12-31 18:23:47 +0900

    add:user registration edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [ :nickname, :gender, :age_group ])
-    devise_parameter_sanitizer.permit(:account_update, keys: [ :nickname, :gender, :age_group ])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :nickname, :gender, :age_group, :started_nail_on ])
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :nickname, :gender, :age_group, :started_nail_on ])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  before_validation :normalize_started_nail_on
   has_many :nail_stocks, dependent: :destroy # 中間テーブルとのアソシエーション
   has_many :nail_items, through: :nail_stocks
   has_many :item_stocks, through: :nail_stocks, source: :nail_item # item_stocksメソッドを定義。nail_stocksを中間テーブルとして、nail_itemsを取得
@@ -28,5 +29,14 @@ class User < ApplicationRecord
   # ユーザーのNail_stocksに、特定のNailItemが登録されているか確認するメソッド
   def nail_stocked?(nail_item)
     item_stocks.include?(nail_item)
+  end
+
+  # ネイルを始めた年月をDate型に正規化する
+  def normalize_started_nail_on
+    return if started_nail_on.blank?
+
+    if started_nail_on.is_a?(String)
+      self.started_nail_on = Date.parse("#{started_nail_on}-01")
+    end
   end
 end
